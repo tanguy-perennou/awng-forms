c02af8c8330c1b833536b3b2e0706166762cef14
Tanguy Pérennou <Tanguy.Perennou@isae-supaero.fr>
2022-04-16 09:32:18 +0200

    Add model PointOfInterest with admin page

--- forms/poi/admin.py
+++ forms/poi/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import PointOfInterest
+
 # Register your models here.
+admin.site.register(PointOfInterest)
--- forms/poi/models.py
+++ forms/poi/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class PointOfInterest(models.Model):
+    """
+    Point of Interest
+    """
+    name = models.CharField(max_length=200)
+    lat = models.FloatField('Latitude (degrees)')
+    lon = models.FloatField('Longitude (degrees)')
+    alt = models.FloatField('Altitude (meters)')
+    
